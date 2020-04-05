8f72e6dd4d0bcad7e34aef606ef70c1a7d26e717
Cédrick COPOL <cedrick.copol@inria.fr>
2020-04-05 18:38:29 +0200

    Correct base Api

--- node_helper.js
+++ node_helper.js
@@ -146,7 +146,7 @@ module.exports = NodeHelper.create({
     var self = this;
     console.log("##### Starting node helper for: " + self.name);
 
-    self.baseApi = 'account.health.nokia.com';
+    self.baseApi = 'account.withings.com';
     self.withingsApi = 'wbsapi.withings.net';
     self.tokenPath = '/oauth2/token';
     self.authorizationUri = self.baseApi + '/oauth2_user/authorize2';
